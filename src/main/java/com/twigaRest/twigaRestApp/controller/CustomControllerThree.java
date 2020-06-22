962a11b9583a60d3ecfcdb92f2117879f620211a
colmike <michaelngigi76@gmail.com>
2020-06-22 10:57:33 +0300

    Add a get all assignments endpoint

---
+++ src/main/java/com/twigaRest/twigaRestApp/controller/CustomControllerThree.java
@@ -0,0 +1,37 @@
+package com.twigaRest.twigaRestApp.controller;
+
+import com.twigaRest.twigaRestApp.dao.AssignmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
+public class CustomControllerThree {
+
+    private AssignmentRepository assignmentRepository;
+
+    @Autowired
+    public CustomControllerThree(AssignmentRepository theAssignmentRepository) {
+        assignmentRepository = theAssignmentRepository;
+    }
+
+
+    @GetMapping("/getAssignments")
+    public String[] getAssignments() {
+
+        System.out.println();
+        return assignmentRepository.getAllAssignments();
+
+    }
+}
+//    @GetMapping("/getTruckById")
+//    public List<Truck> getTruckById(@PathVariable String regNo){
+//    public List<Truck> getTruckById(Model model){
+//        Truck truck = (Truck) truckService.findTruckByRegNumber("KAL 135G");
+//        var trucks = truckRepository.findTruckByRegNumber();
+//
+//        model.addAttribute("trucks", trucks);
+//
+//        System.out.println(trucks);
--- src/main/java/com/twigaRest/twigaRestApp/dao/AssignmentRepository.java
+++ src/main/java/com/twigaRest/twigaRestApp/dao/AssignmentRepository.java
@@ -4,12 +4,18 @@ import com.twigaRest.twigaRestApp.entity.Assignment;
 import com.twigaRest.twigaRestApp.entity.Driver;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
 @CrossOrigin("*")
 public interface AssignmentRepository extends JpaRepository<Assignment, Integer> {
+
+//    @Query(value = "SELECT distinct a.odometerStart, a.driverId FROM Assignment a", nativeQuery = false)
+//    @Query(value = "SELECT DISTINCT id, model, reg_number,travel_status FROM Truck WHERE reg_number='KBE 739V'", nativeQuery = true)
+    @Query(value = "SELECT DISTINCT id, odometer_start, driver_id FROM Assignment WHERE trip_status='0'", nativeQuery = true)
+    public String[] getAllAssignments();
 
 
 }
