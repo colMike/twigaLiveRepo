5b5fb7adef3c6d7950653c3f34dacb19c15f3041
colmike <michaelngigi76@gmail.com>
2020-06-23 15:47:03 +0300

    changed assignments being returned

--- src/main/java/com/twigaRest/twigaRestApp/dao/AssignmentRepository.java
+++ src/main/java/com/twigaRest/twigaRestApp/dao/AssignmentRepository.java
@@ -14,7 +14,7 @@ public interface AssignmentRepository extends JpaRepository<Assignment, Integer>
 
 //    @Query(value = "SELECT distinct a.odometerStart, a.driverId FROM Assignment a", nativeQuery = false)
 //    @Query(value = "SELECT DISTINCT id, model, reg_number,travel_status FROM Truck WHERE reg_number='KBE 739V'", nativeQuery = true)
-    @Query(value = "SELECT DISTINCT id, odometer_start, driver_id FROM Assignment WHERE trip_status='0'", nativeQuery = true)
+    @Query(value = "SELECT DISTINCT id, odometer_start, driver_id FROM Assignment WHERE trip_status='1'", nativeQuery = true)
     public String[] getAllAssignments();
 
 
